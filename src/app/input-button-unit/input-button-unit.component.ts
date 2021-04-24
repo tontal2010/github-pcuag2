8e120aa39923b2f6b0e1c9a708902590453b3302
tontal2010 <tontal27@gmail.com>
2021-04-24 19:11:00 +0700

    Initial commit

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <input class="todo-input"
+           #inputElementRef
+           [value]="title"
+           (keyup.enter)="submitValue($event.target.value)">
+    <button class="btn"
+            (click)="submitValue(inputElementRef.value)">
+      Save
+    </button>
+  `,
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+  @Output() submit: EventEmitter<string> = new EventEmitter<string>();
+  title = 'Hello World';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submitValue(newTitle: string): void {
+    this.submit.emit(newTitle);
+  }
+}
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-todo-item',
+  template: `
+    <div class="todo-item">
+      <div>
+        <input type="checkbox"
+               class="todo-checkbox"
+               (click)="completeItem()"
+               [checked]="item.completed"/>
+        <span class="todo-title" [ngClass]="{'todo-complete': item.completed}">
+          {{ item.title }}
+        </span>
+      </div>
+
+      <button class="btn btn-red" (click)="removeItem()">
+        remove
+      </button>
+    </div>
+  `,
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  removeItem(): void {
+    this.remove.emit(this.item);
+  }
+
+  completeItem(): void {
+    this.update.emit({
+      item: this.item,
+      changes: { completed: !this.item.completed}
+    });
+  }
+
+}
